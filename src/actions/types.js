cb19bb1727c4df28bba2b8a0100d5ecdfd1ba4f4
Eric Abenojar <abenoeric@gmail.com>
2018-11-14 21:43:03 -0600

    Working on types file in actions

--- src/actions/types.js
+++ src/actions/types.js
@@ -5,3 +5,4 @@ export const EDIT_LIST = "EDIT_LIST";
 export const EDIT_CARD = "EDIT_CARD";
 export const DELETE_CARD = "DELETE_CARD";
 export const DELETE_LIST = "DELETE_LIST";
+export const ERROR_CARD = "ERROR_CARD";
